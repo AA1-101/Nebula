61b5dd09d3b74f8224360cf775c523218a2c2535
AA1-101 <arhamaffan13@gmail.com>
2026-08-04 00:55:31 +0500

    Added shorcut to start game (Alt + S)

--- Patches/DiscordPatch.cs
+++ Patches/DiscordPatch.cs
@@ -6,14 +6,14 @@ namespace Nebula.Patches;
 
 // Originally from "Town of Us Rewritten", by Det
 [HarmonyPatch(typeof(ActivityManager), nameof(ActivityManager.UpdateActivity))]
-public static class DiscordRPC
+public static class DiscordPatch
 {
     private static string Lobbycode = "";
     private static string Region = "";
-    
+    [HarmonyPrefix]
     public static void Prefix([HarmonyArgument(0)] Activity activity)
     {
-        Main.Logger.LogInfo("DiscordRpcPatch called");
+        Main.Logger.LogInfo("DiscordPatch called");
         if (activity == null) return;
 
         var details = $"Nebula v{Main.PluginDisplayVersion}";
@@ -21,7 +21,7 @@ public static class DiscordRPC
 
         activity.Assets = new ActivityAssets
         {
-            LargeImage = "https://i.ibb.co/Q3r7nKCV/file-000000005f9472079c2f406e2ae9a354.png"
+            LargeImage = "https://i.ibb.co/Q3r7nKCV/file-00B0000005f9472079c2f406e2ae9a354.png"
         };
 
         try
@@ -36,7 +36,7 @@ public static class DiscordRPC
                         Region = Modules.Utils.GetRegionName();
                     }
 
-                    if (Lobbycode != "" && Region != "") details = $"EHR - {Lobbycode} ({Region})";
+                    if (Lobbycode != "" && Region != "") details = $"Nebula - {Lobbycode} ({Region})";
                 }
                 else
                     details = $"Nebula v{Main.PluginDisplayVersion}";
---
+++ Patches/GameStartManagerPatches.cs
@@ -0,0 +1,36 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace Nebula.Patches
+{
+    [HarmonyPatch(typeof(GameStartManager), nameof(GameStartManager.Start))]
+    public class MinPlayerPatch
+    {
+        public static void Postfix()
+        {
+            GameStartManager.Instance.MinPlayers = 1;
+        }
+    }
+    [HarmonyPatch(typeof(GameStartManager), nameof (GameStartManager.Update))]
+
+    public class StartGameShortcut
+    {
+        public static void Postfix()
+        {
+            if (!AmongUsClient.Instance.AmHost)
+                return;
+
+            // compare the instance startState to the enum values
+            if (GameStartManager.Instance.startState == GameStartManager.StartingStates.Starting
+                || GameStartManager.Instance.startState == GameStartManager.StartingStates.Countdown)
+                return;
+
+            // ensure Alt + S is required (fix precedence)
+            if ((Input.GetKey(KeyCode.LeftAlt) || Input.GetKey(KeyCode.RightAlt))
+                && Input.GetKeyDown(KeyCode.S))
+            {
+                GameStartManager.Instance.BeginGame();
+            }
+        }
+    }
+}
