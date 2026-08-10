bf0c2089e23f5617242dc6c07935842d0c58d948
AA1-101 <arhamaffan13@gmail.com>
2026-08-10 21:56:32 +0500

    reponsibility change

--- Patches/ChatCommandPatch.cs
+++ Patches/ChatCommandPatch.cs
@@ -7,8 +7,11 @@ namespace Nebula.Patches
     [HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.RpcSendChat))]
     public static class ChatCommandPatch
     {
-        public static bool Prefix(PlayerControl __instance,string chatText)
+        public static bool Prefix(PlayerControl __instance, string chatText)
         {
+            if (!AmongUsClient.Instance.AmHost)
+                return true;
+
             if (!chatText.StartsWith("/cmd", StringComparison.OrdinalIgnoreCase))
                 return true;
 
@@ -16,28 +19,12 @@ namespace Nebula.Patches
 
             Main.Logger.LogInfo($"Chat intercepted: {chatText}");
 
-            Utils.CheckServerCommand(ref chatText);
-
-            chatText = chatText[1..];
-
-            string[] split = chatText.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-
-            if (split.Length == 0)
-            {
-                RpcSender.SendMessage(
-                    __instance,
-                    "Usage: /cmd <command>\nTry using /cmd help",
-                    sendTo: __instance.OwnerId);
-
-                return false;
-            }           
-
-            string command = split[0].ToLowerInvariant();
-            string[] args = split.Skip(1).ToArray();
-
-            CommandManager.HandleCommand(__instance, command, args);
-
-            return false;
-        }
+            return CommandManager.OnReceiveChat(__instance, chatText);            
+        }        
     }
 }
+
+
+        
+    
+
