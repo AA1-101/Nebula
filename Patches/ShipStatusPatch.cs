46abae86d8c397cea2ef7f48811010d8ca9f6b58
AA1-101 <arhamaffan13@gmail.com>
2026-08-10 21:56:59 +0500

    new class and shortcut key!!!

---
+++ Patches/ShipStatusPatch.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using Nebula.Modules;
+
+namespace Nebula.Patches
+{
+    [HarmonyPatch(typeof(ShipStatus), nameof(ShipStatus.FixedUpdate))]
+    public static class ShipStatusPatch
+    {
+        public static void Postfix()
+        {
+            if (!AmongUsClient.Instance.AmHost)
+                return;
+
+            if (GameStates.IsInLobby)
+                return;
+
+            if (GameStates.IsInMeeting)
+                return;
+
+            if (Input.GetKey(KeyCode.LeftControl)
+                &&( Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+                && Input.GetKeyDown(KeyCode.P))
+            {
+                PlayerControl.LocalPlayer.RpcStartMeeting(null);
+            }
+        }
+    }
+}
