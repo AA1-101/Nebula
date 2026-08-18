5f36e2bbbf6469ee5e44b270dfea71ac99d1db9c
AA1-101 <arhamaffan13@gmail.com>
2026-08-18 20:14:09 +0500

    forgor

--- Patches/OnGameJoinedPatch.cs
+++ Patches/OnGameJoinedPatch.cs
@@ -1,18 +1,15 @@
 ﻿using HarmonyLib;
+using Nebula;
 using Nebula.Modules;
 using Nebula.Networking;
-
-namespace Nebula.Patches;
 
 [HarmonyPatch(typeof(AmongUsClient), nameof(AmongUsClient.OnGameJoined))]
 public static class OnGameJoinedPatch
 {
     public static void Postfix()
-    {
-        DisplayTagManager.OriginalNames.Clear();
-
+    {     
         Main.Instance.StartCoroutine(DisplayTagManager.DisplayName());
-    }
+
+        Main.Instance.StartCoroutine(RpcSender.SendHandshakeWhenReady());
+    }    
 }
-
-
